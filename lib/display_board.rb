59e1c366a74acd6f8878c93dcde1c153ba6936a5
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-02 20:29:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@
 turn_1 = ["X", " "," "," "," "," "," "," "," "]
 
 def display_board (the_board)
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 display_board(turn_1)
